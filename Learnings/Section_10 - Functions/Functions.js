040bbc7fff56a1e31e80477fea3cc848f4b04a15
SC-Ponce <ponceselwyne0128@gmail.com>
2025-04-15 05:50:22 +0800

    update

--- Learnings/Section_10 - Functions/Functions.js
+++ Learnings/Section_10 - Functions/Functions.js
@@ -178,9 +178,9 @@ luftansia.addPlane = function () {
   console.log(this.numPlane);
 };
 
-document
-  .querySelector(".buy")
-  .addEventListener("click", luftansia.addPlane.bind(luftansia));
+// document
+//   .querySelector(".buy")
+//   .addEventListener("click", luftansia.addPlane.bind(luftansia));
 
 /* 
 Let's build a simple poll app!
@@ -241,8 +241,101 @@ const poll = {
   },
 };
 
-document
-  .querySelector(".poll")
-  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+// document
+//   .querySelector(".poll")
+//   .addEventListener("click", poll.registerNewAnswer.bind(poll));
 
-poll.displayResult.call({ answer: [5, 3, 2, 1] });
+// poll.displayResult.call({ answer: [5, 3, 2, 1] });
+
+//Immediately Invoked Functions Expressions
+
+//Normal functions - IIFE
+(function () {
+  console.log("This will never run again!");
+})();
+
+(() => console.log("This too is IIFE"))();
+
+//closures
+const secureBooking = function () {
+  let passengers = 0;
+
+  return function () {
+    passengers++;
+    console.log(`${passengers} passengers`);
+  };
+};
+
+const booking = secureBooking();
+booking();
+booking();
+booking();
+
+//examples
+
+const counter = function () {
+  let count = 0;
+  return {
+    add() {
+      count++;
+    },
+    get() {
+      return count;
+    },
+    reset() {
+      count = 0;
+    },
+  };
+};
+
+const startCount = counter();
+startCount.add();
+startCount.get();
+
+//1
+const createMultiplier = function (multiplier) {
+  return function (number) {
+    return number * multiplier;
+  };
+};
+
+const double = createMultiplier(2);
+const triple = createMultiplier(3);
+
+console.log(double(4));
+console.log(triple(5));
+
+//3
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers!`);
+    console.log(`There are 3 groups, each with ${perGroup} Passengers`);
+  }, wait * 1000);
+
+  console.log(`will start boarding in ${wait} seconds!`);
+};
+
+boardPassengers(180, 3);
+
+//////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  document.querySelector("body").addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
